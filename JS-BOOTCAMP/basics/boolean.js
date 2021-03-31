03aee559f21740ab6c713910532dec37bf8cbf3e
Ben McEwen <bdmcewen.bm@gmail.com>
2021-03-31 09:16:32 -0600

    booleans abd if statement

---
+++ JS-BOOTCAMP/basics/boolean.js
@@ -0,0 +1,23 @@
+let is_freezing = false
+let temp = 101
+
+//boolean
+is_freezing = temp < 32
+
+
+//if statement
+if (temp < 32) {
+  is_freezing = true
+}
+
+//advanced if statement if else & else if
+if (temp<32) {
+  is_freezing = true
+  console.log("Burrr, too cold")
+} else if (temp > 100){
+  console.log("Hot Damn!!")
+} else{
+  console.log("Nice.")
+}
+
+// console.log(is_freezing)
