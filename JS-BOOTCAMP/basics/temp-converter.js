3dac8f3e4c7e6fa2232d9625e7105c47b4767942
Ben McEwen <bdmcewen.bm@gmail.com>
2021-03-31 08:51:52 -0600

    unit 1 challenge

---
+++ JS-BOOTCAMP/basics/temp-converter.js
@@ -0,0 +1,18 @@
+// Challenge app to convert a temperature value  Celsius and Kelvin. 
+
+let temp_f = 50 // 0c - 273.15k
+
+// convert to Celsius
+let temp_c = (temp_f-32) * 5 / 9
+
+// convert to Kelvin
+let temp_k = (temp_f + 459.67) * 5 / 9 
+
+console.log("Fahrenheit = " + temp_f)
+console.log("Celsius = " + temp_c)
+console.log("Kelvin = " + temp_k)
+
+
+
+
+
